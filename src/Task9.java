b8c86293ad82432e48aba0173f73b26e90c5135c
alishershayakhmetov <230785@astanait.edu.kz>
2024-04-02 22:54:07 +0500

    problem 9 solved

---
+++ src/Task9.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        System.out.println(binomialCoefficient(n,k));
+    }
+
+    public static int binomialCoefficient(int n, int k) {
+        if (k == 0 || k == n) {
+            return 1;
+        }
+
+        return binomialCoefficient(n - 1, k - 1) + binomialCoefficient(n - 1, k);
+    }
+}
